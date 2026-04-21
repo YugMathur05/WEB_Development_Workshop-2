1596b6ff57e86c7b3576f1734ae604dd428b83af
YugMathur05 <yug.m0578@gmail.com>
2026-04-21 18:09:07 +0530

    Adding name and roll no in code

--- EXP-4/Exp-4.js
+++ EXP-4/Exp-4.js
@@ -1,43 +1,33 @@
 function calculateResult() {
     let n = document.getElementById("subjects").value;
     let total = 0;
-
-    for (let i = 1; i <= n; i++) {
+   for (let i = 1; i <= n; i++) {
         let marks = parseFloat(prompt("Enter the marks for subject " + i));
         total = total + marks;
     }
-
     let avg = total / n;
     let grade;
     let result;
 
     if (avg >= 90) {
-        grade = "A+";
-    }
+        grade = "A+"; }
     else if (avg >= 75) {
-        grade = "A";
-    }
+        grade = "A"; }
     else if (avg >= 60) {
-        grade = "B";
-    }
+        grade = "B"; }
     else if (avg >= 50) {
-        grade = "C";
-    }
+        grade = "C"; }
     else if (avg >= 40) {
-        grade = "D";
-    }
+        grade = "D"; }
     else {
-        grade = "F";
-    }
-
+        grade = "F"; }
     if (avg >= 40) {
         result = "Pass";
     }
     else {
         result = "Fail";
     }
-
-    document.getElementById("result").innerHTML =
+ document.getElementById("result").innerHTML =
         "Total Marks: " + total + "<br>" +
         "Average Marks: " + avg.toFixed(2) + "<br>" +
         "Grade: " + grade + "<br>" +
