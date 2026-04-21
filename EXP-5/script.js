f8ad7349935bd2c58ae73e59407c995d0fd51d7c
YugMathur05 <yug.m0578@gmail.com>
2026-04-21 18:09:38 +0530

    Adding name and roll no in code

--- EXP-5/script.js
+++ EXP-5/script.js
@@ -7,39 +7,33 @@ let displayBtn = document.getElementById("DisplayAll");
 let greaterBtn = document.getElementById("SalaryGreater");
 let avgBtn = document.getElementById("Avg");
 let countBtn = document.getElementById("Count");
-// Add Employee
 addBtn.addEventListener("click", () => {
     let name = inputs[0].value.trim();
     let id = inputs[1].value.trim();
     let salary = inputs[2].value.trim();
     let dept = inputs[3].value.trim();
-// ✅ Validation
+
     if (name === "" || id === "" || salary === "" || dept === "") {
-        alert("⚠️ Please fill all employee details!");
+        alert(" Please fill all employee details!");
         return;
     }
-
     let emp = {
         name: name,
         id: id,
         salary: Number(salary),
         dept: dept
     };
-
     employees.push(emp);
 
-    output.innerHTML = "✅ Employee Added Successfully";
+    output.innerHTML = " Employee Added Successfully";
 
     inputs.forEach(input => input.value = "");
 });
 
-// Total Salary
 totalBtn.addEventListener("click", () => {
     let total = employees.reduce((sum, e) => sum + e.salary, 0);
     output.innerHTML = `<b>Total Salary:</b> ${total}`;
 });
-
-// Display All
 displayBtn.addEventListener("click", () => {
     let html = "<h3>All Employees:</h3>";
 
@@ -49,8 +43,6 @@ displayBtn.addEventListener("click", () => {
 
     output.innerHTML = html;
 });
-
-// Salary > 50k
 greaterBtn.addEventListener("click", () => {
     let result = employees.filter(e => e.salary > 50000);
 
@@ -61,16 +53,12 @@ greaterBtn.addEventListener("click", () => {
 
     output.innerHTML = html;
 });
-
-// Average Salary
 avgBtn.addEventListener("click", () => {
     let total = employees.reduce((sum, e) => sum + e.salary, 0);
     let avg = employees.length ? total / employees.length : 0;
 
     output.innerHTML = `<b>Average Salary:</b> ${avg}`;
 });
-
-// Count by Department
 countBtn.addEventListener("click", () => {
     let deptCount = {};
 
